2d086dc9ec7cc4128db21ca28e73e2415aee28cb
GuinhoIFPR <macharetevic@gmail.com>
2023-08-16 12:04:26 -0300

    Exercicio 3

--- Exerc3/Exerc3.php
+++ Exerc3/Exerc3.php
@@ -1,4 +1,8 @@
-<form action="">
+<form action="" method="get">
+    <label> Digite seu peso: </label>
+    <input type="number" name="peso">
+    <label> Digite sua altura:</label>
+    <input type="number" name="altura">
 
-
+    <input type="submit">
 </form>
